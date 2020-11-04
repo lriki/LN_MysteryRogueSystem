552a7d64db30bb685f2c1b4983f0321533098f0b
lriki <lriki.net@gmail.com>
2020-11-04 21:13:13 +0900

    iterationCount

--- test/Basic.test.ts
+++ test/Basic.test.ts
@@ -201,27 +201,42 @@ test('TurnOrderTable', () => {
         const run0 = runs[0].steps;
         expect(run0.length).toBe(5);
         expect(run0[0].unit.unit).toEqual(actor1);  // 先頭は Player
+        expect(run0[0].iterationCount).toEqual(1);
         expect(run0[1].unit.unit).toEqual(enemy3);  // 以降、x2速以上の Enemy が積まれている
+        expect(run0[1].iterationCount).toEqual(2);
         expect(run0[2].unit.unit).toEqual(enemy4);
+        expect(run0[2].iterationCount).toEqual(2);
         expect(run0[3].unit.unit).toEqual(enemy5);
+        expect(run0[3].iterationCount).toEqual(3);
         expect(run0[4].unit.unit).toEqual(enemy6);
+        expect(run0[4].iterationCount).toEqual(3);
 
         const run1 = runs[1].steps;
         expect(run1.length).toBe(4);
         expect(run1[0].unit.unit).toEqual(enemy3);  // 以降、x2速以上の Enemy が積まれている
+        expect(run1[0].iterationCount).toEqual(0);
         expect(run1[1].unit.unit).toEqual(enemy4);
+        expect(run1[1].iterationCount).toEqual(0);
         expect(run1[2].unit.unit).toEqual(enemy5);
+        expect(run1[2].iterationCount).toEqual(0);
         expect(run1[3].unit.unit).toEqual(enemy6);
+        expect(run1[3].iterationCount).toEqual(0);
 
         // 最後の Run には、x2速以上の余りと、x1速以下の Entity が積まれている
         const run2 = runs[2].steps;
         expect(run2.length).toBe(6);
         expect(run2[0].unit.unit).toEqual(enemy5);  // x3 優先
+        expect(run2[0].iterationCount).toEqual(0);
         expect(run2[1].unit.unit).toEqual(enemy6);  // x3 優先
+        expect(run2[1].iterationCount).toEqual(0);
         expect(run2[2].unit.unit).toEqual(enemy1);  // x1
+        expect(run2[2].iterationCount).toEqual(1);
         expect(run2[3].unit.unit).toEqual(enemy2);  // x1
+        expect(run2[3].iterationCount).toEqual(1);
         expect(run2[4].unit.unit).toEqual(enemy7);  // x0.5 鈍足でも x1 と同じく、行動予定は積む
+        expect(run2[4].iterationCount).toEqual(1);
         expect(run2[5].unit.unit).toEqual(enemy8);  // x0.5 鈍足でも x1 と同じく、行動予定は積む
+        expect(run2[5].iterationCount).toEqual(1);
     }
 
     const commandContext = REGame.scheduler.commandContext();
