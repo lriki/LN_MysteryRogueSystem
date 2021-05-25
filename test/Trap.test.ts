bdec434f51a42d56e979858d620d42e873ddad8d
lriki <lriki.net@gmail.com>
2021-05-25 23:37:06 +0900

    trap

--- test/Trap.test.ts
+++ test/Trap.test.ts
@@ -46,5 +46,36 @@ test("Trap.Basic", () => {
     
     RESystem.scheduler.stepSimulation(); // Advance Simulation --------------------------------------------------
 
-    expect(actor1.states()[0].stateId()).toBe(TestEnv.StateId_Sleep);
+    expect(actor1.states()[0].stateId()).toBe(TestEnv.StateId_Sleep);   // 睡眠状態
+});
+
+test("Trap.Enemy", () => {
+    SGameManager.createGameObjects();
+
+    // actor1 配置
+    const actor1 = REGame.world.entity(REGame.system.mainPlayerEntityId);
+    actor1._name = "actor1";
+    REGame.world._transferEntity(actor1, TestEnv.FloorId_FlatMap50x50, 10, 10);
+    TestEnv.performFloorTransfer();
+
+    // trap1 生成&配置
+    const trap1 = SEntityFactory.newEntity({ prefabId: TestEnv.PrefabId_SleepTrap, stateIds: [] });
+    trap1._name = "trap1";
+    REGame.world._transferEntity(trap1, TestEnv.FloorId_FlatMap50x50, 11, 10);
+    // TODO: 罠state:必ず発動
+
+    // enemy1
+    const enemy1 = SEntityFactory.newMonster(1);
+    enemy1._name = "enemy1";
+    REGame.world._transferEntity(enemy1, TestEnv.FloorId_FlatMap50x50, 12, 10);
+
+    RESystem.scheduler.stepSimulation(); // Advance Simulation --------------------------------------------------
+    
+    // 足踏み
+    const dialogContext = RESystem.dialogContext;
+    dialogContext.activeDialog().submit(DialogSubmitMode.ConsumeAction);
+    
+    RESystem.scheduler.stepSimulation(); // Advance Simulation --------------------------------------------------
+
+    expect(enemy1.states().length).toBe(0); // Enemy は罠にはかからないこと
 });
