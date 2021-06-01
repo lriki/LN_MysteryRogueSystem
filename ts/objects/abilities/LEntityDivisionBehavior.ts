2fb05d7e813f95efd80fbe8b9fa3119b7538ab42
lriki <lriki.net@gmail.com>
2021-06-01 14:12:10 +0900

    added LEntityDivisionBehavior

---
+++ ts/objects/abilities/LEntityDivisionBehavior.ts
@@ -0,0 +1,10 @@
+import { REResponse } from "ts/system/RECommand";
+import { SCommandContext } from "ts/system/SCommandContext";
+import { LProjectableBehavior } from "../behaviors/activities/LProjectableBehavior";
+import { CollideActionArgs, CommandArgs, LBehavior, onCollideAction } from "../behaviors/LBehavior";
+
+
+export class LEntityDivisionBehavior extends LBehavior {
+    
+}
+
