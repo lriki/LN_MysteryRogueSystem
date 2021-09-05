b87512fcf9f22cd9b28c3794483a1949ce7a1031
lriki <lriki.net@gmail.com>
2021-09-05 15:00:11 +0900

    refactor: LMoveDeterminer

--- ts/objects/ai/LStandardAI.ts
+++ ts/objects/ai/LStandardAI.ts
@@ -15,6 +15,7 @@ import { LActivity } from "./../activities/LActivity";
 import { SEmittorPerformer } from "ts/system/SEmittorPerformer";
 import { LCharacterAI } from "./LCharacterAI";
 import { LActionDeterminer } from "./LActionDeterminer";
+import { LMoveDeterminer } from "./LMoveDeterminer";
 
 /**
  * https://yttm-work.jp/game_ai/game_ai_0001.html
@@ -22,24 +23,14 @@ import { LActionDeterminer } from "./LActionDeterminer";
  */
 export class LCharacterAI_Normal extends LCharacterAI {
     
-    
-    // 移動ターゲットとなる座標。
-    // _primaryTargetEntity ではなく、部屋の入り口などを示すこともある。
-    private _targetPositionX: number = -1;
-    private _targetPositionY: number = -1;
 
+    private _moveDeterminer = new LMoveDeterminer();
     private _actionDeterminer = new LActionDeterminer();
-
-
-    private _noActionTurnCount: number = 0;
 
     public clone(): LCharacterAI {
         const i = new LCharacterAI_Normal();
-        i._targetPositionX = this._targetPositionX;
-        i._targetPositionY = this._targetPositionY;
+        i._moveDeterminer = this._moveDeterminer.clone();
         i._actionDeterminer = this._actionDeterminer.clone();
-        //i._attackTargetEntityId = this._attackTargetEntityId.clone();
-        i._noActionTurnCount = this._noActionTurnCount;
         return i;
     }
 
@@ -53,8 +44,7 @@ export class LCharacterAI_Normal extends LCharacterAI {
         // 攻撃目標が設定されていれば、それを移動目標とする
         if (this._actionDeterminer.hasPrimaryTarget()) {
             const target = this._actionDeterminer.primaryTarget();
-            this._targetPositionX = target.x;
-            this._targetPositionY = target.y;
+            this._moveDeterminer.setTargetPosition(target.x, target.y);
         }
 
         // 攻撃対象が設定されていれば、このフェーズでは何もしない
@@ -64,8 +54,7 @@ export class LCharacterAI_Normal extends LCharacterAI {
         
 
         // 移動メイン
-        if (this.thinkMoving_Search(self, context)) {
-            this._noActionTurnCount = 0;
+        if (this._moveDeterminer.perform(context, self)) {
             return SPhaseResult.Handled;
         }
 
@@ -159,155 +148,6 @@ export class LCharacterAI_Normal extends LCharacterAI {
         }
     }
     */
-
-    // 目的地あり？
-    private hasDestination(): boolean {
-        return this._targetPositionX >= 0 && this._targetPositionY >= 0;
-    }
-
-    // 
-    private thinkMoving_Search(self: LEntity, context: SCommandContext): boolean {
-        let moveToLHRule = false;
-        let moveToPassageWay: LBlock | undefined;
-        const block = REGame.map.block(self.x, self.y);
-
-        if (!this.hasDestination()) {
-            if (!block.isRoom()) {
-                // 目的地なし, 現在位置が通路・迷路
-                // => 左折の法則による移動
-                moveToLHRule = true;
-            }
-            else {
-                const room = REGame.map.room(block._roomId);
-                if (!block.isDoorway()) {
-                    // 目的地なし, 現在位置が部屋
-                    // => ランダムな入口を目的地に設定し、目的地に向かう移動。
-                    // => 入口が無ければ左折の法則による移動
-
-                    const candidates = room.doorwayBlocks();
-                    if (candidates.length > 0) {
-                        const block = candidates[context.random().nextIntWithMax(candidates.length)];
-                        this._targetPositionX = block.x();
-                        this._targetPositionY = block.y();
-                    }
-                    else {
-                        // 入り口のない部屋。左折の法則による移動を継続する。
-                        moveToLHRule = true;
-                    }
-                }
-                else {
-                    // 目的地なし, 現在位置が部屋の入口
-                    // => 現在位置以外のランダムな入口を目的地に設定し、左折の法則による移動
-                    // => 他に入口がなければ逆方向を向き、左折の法則による移動
-                    moveToLHRule = true;
-                    
-    
-                    const candidates = room.doorwayBlocks().filter(b => b.x() != self.x && b.y() != self.y);    // 足元フィルタ
-                    if (candidates.length > 0) {
-                        const block = candidates[context.random().nextIntWithMax(candidates.length)];
-                        this._targetPositionX = block.x();
-                        this._targetPositionY = block.y();
-                    }
-                    else {
-                        self.dir = UMovement.reverseDir(self.dir);
-                    }
-                }
-            } 
-        }
-        else if (!this.canModeToTarget(self)) {
-            // 目的地あり 目的地が現在位置
-            // => 目的地を解除し、左折の法則による移動
-            this._targetPositionX = -1;
-            this._targetPositionY = -1;
-
-            // これは SFC シレン Wiki には乗っていない細工。
-            // 部屋内から目的地にたどり着いたとき、現在の向きと通路の方向が直角だと、左折の法則で通路に侵入できなくなる。
-            // 対策として、このときは隣接している通路ブロックへの移動を優先する。
-            const blocks = UMovement.getMovableAdjacentTiles(self).filter(b => b.isPassageway());
-            if (blocks.length > 0) {
-                moveToPassageWay = blocks[context.random().nextIntWithMax(blocks.length)];
-            }
-            else {
-                moveToLHRule = true;
-            }
-        }
-        else {
-            // 目的地あり 目的地が現在位置でない
-            // => 目的地に向かう移動 (moveToTarget() で移動)
-        }
-
-        // 目的地設定がなされてるのであればそこへ向かって移動する
-        if (this.canModeToTarget(self)) {
-            if (this.moveToTarget(self, context)) {
-                return true;
-            }
-            else {
-                // 壁際を斜め移動しようとした等、移動できなかった
-                moveToLHRule = true;
-            }
-        }
-
-        if (moveToPassageWay) {
-            this.postMoveToAdjacent(self, moveToPassageWay, context);
-            return true;
-        }
-
-        // 左折の法則による移動
-        if (moveToLHRule) {
-            const block = UMovement.getMovingCandidateBlockAsLHRule(self, self.dir);
-            if (block) {
-                this.postMoveToAdjacent(self, block, context);
-
-                // 移動後、向きを target へ向けておく
-                const dir = SAIHelper.distanceToDir(self.x, self.y, this._targetPositionX, this._targetPositionY);
-                context.postActivity(LActivity.makeDirectionChange(self, dir));
-
-                return true;
-            }
-        }
-
-        this._noActionTurnCount++;
-        if (this._noActionTurnCount >= 6) {
-            // 6連続で移動できなかったときはランダム移動
-            const candidates = UMovement.getMovableAdjacentTiles(self);
-            if (candidates.length > 0) {
-                const block = candidates[context.random().nextIntWithMax(candidates.length)];
-                this.postMoveToAdjacent(self, block, context);
-                this._noActionTurnCount = 0;
-                return true;
-            }
-        }
-
-        return false;
-    }
-
-    private canModeToTarget(self: LEntity): boolean {
-        return this.hasDestination() && (self.x != this._targetPositionX || self.y != this._targetPositionY);
-    }
-
-    private moveToTarget(self: LEntity, context: SCommandContext): boolean {
-        // 目的地設定済みで、未到達であること
-        assert(this.canModeToTarget(self));
-
-        const dir = SAIHelper.distanceToDir(self.x, self.y, this._targetPositionX, this._targetPositionY);
-        if (dir != 0 && UMovement.checkPassageToDir(self, dir)) {
-            context.postActivity(LActivity.makeDirectionChange(self, dir));
-            context.postActivity(LActivity.makeMoveToAdjacent(self, dir));
-            //this.moveToAdjacent(self, block, context);
-            context.postConsumeActionToken(self);
-            return true;
-        }
-        else {
-            return false;
-        }
-    }
-    
-    private postMoveToAdjacent(self: LEntity, block: LBlock, context: SCommandContext): void {
-        const dir = Helpers.offsetToDir(block.x() - self.x, block.y() - self.y);
-        context.postActivity(LActivity.makeDirectionChange(self, dir));
-        context.postActivity(LActivity.makeMoveToAdjacent(self, dir));
-        context.postConsumeActionToken(self);
-    }
     
     public thinkAction(context: SCommandContext, self: LEntity): SPhaseResult {
         if (this._actionDeterminer.perform(context, self)) {
