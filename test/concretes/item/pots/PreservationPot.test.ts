0b8c8cd9161e49c01de5f8cc57d28cc4abdde2b3
lriki <lriki.net@gmail.com>
2021-10-05 19:52:21 +0900

    docs

---
+++ test/concretes/item/pots/PreservationPot.test.ts
@@ -0,0 +1,36 @@
+import { LInventoryBehavior } from "ts/re/objects/behaviors/LInventoryBehavior";
+import { REGame } from "ts/re/objects/REGame";
+import { SEntityFactory } from "ts/re/system/SEntityFactory";
+import { RESystem } from "ts/re/system/RESystem";
+import { TestEnv } from "../../../TestEnv";
+import { REData } from "ts/re/data/REData";
+import { DEntityCreateInfo } from "ts/re/data/DEntity";
+import { LActivity } from "ts/re/objects/activities/LActivity";
+import { TestUtils } from "test/TestUtils";
+import { LGoldBehavior } from "ts/re/objects/behaviors/LGoldBehavior";
+import { REBasics } from "ts/re/data/REBasics";
+import { SFormulaOperand } from "ts/re/system/SFormulaOperand";
+import { LActionTokenType } from "ts/re/objects/LActionToken";
+
+beforeAll(() => {
+    TestEnv.setupDatabase();
+});
+
+test("concretes.item.pots.PreservationPot.Basic", () => {
+    /*
+    TestEnv.newGame();
+
+    // Player
+    const actor1 = TestEnv.setupPlayer(TestEnv.FloorId_UnitTestFlatMap50x50, 10, 10);
+    const inventory1 = actor1.getEntityBehavior(LInventoryBehavior);
+    
+    const item1 = SEntityFactory.newEntity(DEntityCreateInfo.makeSingle( REData.getEntity("kItem_保存の壺").id, [], "item1"));
+    const item2 = SEntityFactory.newEntity(DEntityCreateInfo.makeSingle( REData.getEntity("kキュアリーフ").id, [], "item2"));
+    inventory1.addEntity(item1);
+    inventory1.addEntity(item2);
+
+
+    RESystem.scheduler.stepSimulation(); // Advance Simulation --------------------------------------------------
+    */
+});
+
