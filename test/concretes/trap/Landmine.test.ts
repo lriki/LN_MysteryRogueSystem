3498821a4c333eb8969706c6d32477c8b567967a
lriki <lriki.net@gmail.com>
2021-10-25 18:18:22 +0900

    feat: landmine

--- test/concretes/trap/Landmine.test.ts
+++ test/concretes/trap/Landmine.test.ts
@@ -5,22 +5,21 @@ import { TestEnv } from "../../TestEnv";
 import { REData } from "ts/re/data/REData";
 import { DEntityCreateInfo } from "ts/re/data/DEntity";
 import { LActivity } from "ts/re/objects/activities/LActivity";
-import { LInventoryBehavior } from "ts/re/objects/behaviors/LInventoryBehavior";
 import { REBasics } from "ts/re/data/REBasics";
 
 beforeAll(() => {
     TestEnv.setupDatabase();
 });
 
-test("concretes.trap.Landmine", () => {
+test("concretes.trap.Landmine.basic", () => {
     TestEnv.newGame();
 
     // Player
     const player1 = TestEnv.setupPlayer(TestEnv.FloorId_FlatMap50x50, 10, 10);
-    const hp1 = 10;
+    const hp1 = 8;
     player1.setActualParam(REBasics.params.hp, hp1);    // テストしやすいように、割り切れる HP にしておく
 
-    // trap1 生成&配置
+    // trap 生成&配置
     const trap1 = SEntityFactory.newEntity(DEntityCreateInfo.makeSingle(REData.getEntity("kItem_地雷").id, [], "trap1"));
     REGame.world._transferEntity(trap1, TestEnv.FloorId_FlatMap50x50, 11, 10);
 
@@ -34,10 +33,44 @@ test("concretes.trap.Landmine", () => {
     
     REGame.world.random().resetSeed(5);     // 乱数調整
     RESystem.scheduler.stepSimulation();    // Advance Simulation ----------
-
     
     const hp2 = player1.actualParam(REBasics.params.hp);
     expect(hp2).toBe(hp1 / 2);  // HP半分になっている
     expect(player1.isDestroyed()).toBe(false);  // 消滅していないこと
     expect(trap1.isDestroyed()).toBe(false);    // 消滅していないこと
 });
+
+/*
+test("concretes.trap.Landmine.InducedExplosion", () => {
+    TestEnv.newGame();
+
+    // Player
+    const player1 = TestEnv.setupPlayer(TestEnv.FloorId_FlatMap50x50, 10, 10);
+    const hp1 = 8;
+    player1.setActualParam(REBasics.params.hp, hp1);    // テストしやすいように、割り切れる HP にしておく
+
+    // trap 生成&配置
+    const trap1 = SEntityFactory.newEntity(DEntityCreateInfo.makeSingle(REData.getEntity("kItem_地雷").id, [], "trap1"));
+    const trap2 = SEntityFactory.newEntity(DEntityCreateInfo.makeSingle(REData.getEntity("kItem_地雷").id, [], "trap2"));
+    REGame.world._transferEntity(trap1, TestEnv.FloorId_FlatMap50x50, 11, 10);
+    REGame.world._transferEntity(trap2, TestEnv.FloorId_FlatMap50x50, 11, 11);
+
+    RESystem.scheduler.stepSimulation();    // Advance Simulation ----------
+    
+    //----------------------------------------------------------------------------------------------------
+
+    // player を右 (罠上) へ移動
+    RESystem.dialogContext.postActivity(LActivity.makeMoveToAdjacent(player1, 6).withEntityDirection(6).withConsumeAction());
+    RESystem.dialogContext.activeDialog().submit();
+    
+    REGame.world.random().resetSeed(5);     // 乱数調整
+    RESystem.scheduler.stepSimulation();    // Advance Simulation ----------
+    
+    const hp2 = player1.actualParam(REBasics.params.hp);
+    expect(hp2).toBe(hp1 / 4);  // HP半分を２回受けている
+    expect(player1.isDestroyed()).toBe(false);  // 消滅していないこと
+    expect(trap1.isDestroyed()).toBe(false);    // 消滅していないこと
+    expect(trap2.isDestroyed()).toBe(false);    // 消滅していないこと
+});
+*/
+
